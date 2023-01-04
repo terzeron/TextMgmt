27a60151de469b87f98892f0c6dfe550386f3994
Youngil Cho <terzeron@gmail.com>
2023-01-04 14:18:09 +0900

    update startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -2,10 +2,12 @@
 
 pidfile="uvicorn.pid"
 if [ -f "$pidfile" ]; then
-    echo "Killing old process"
+    echo "Killing old process..."
     kill $(cat $pidfile)
     sleep 3
 fi
 
+echo "Starting service..."
 nohup uvicorn main:app --workers=2 &
 echo $! > uvicorn.pid
+sleep 2
