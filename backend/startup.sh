f5c1f79fef5ced12416478b2e433e792bb98e0bb
Youngil Cho <terzeron@gmail.com>
2025-07-24 16:25:23 +0900

    startup.sh 스크립트 수정: uvicorn 워커 수를 10에서 4로 변경하고, nohup.out 파일을 tail -f로 출력하도록 수정

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+pwd
+
 pidfile="uvicorn.pid"
 if [ -f "$pidfile" ]; then
     echo "Killing old process..."
     kill $(cat $pidfile)
-    sleep 3
+    sleep 2
 fi
 
 rm -f nohup.out
 echo "Starting service..."
-nohup uvicorn main:app --workers=10 &
+nohup uvicorn main:app --workers=4 &
 echo "$!" > "$pidfile"
 sleep 2
-tail nohup.out
+tail -f nohup.out
