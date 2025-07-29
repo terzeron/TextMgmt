14835acde3765f98c5b262408694bc0adf4b8cb9
Youngil Cho <terzeron@gmail.com>
2025-07-29 14:57:23 +0900

    insert 메서드의 배치 크기를 100에서 100으로 변경하여 데이터 삽입 성능을 개선함.

--- backend/es_manager.py
+++ backend/es_manager.py
@@ -276,11 +276,11 @@ class ESManager:
         LOGGER.debug("insert() %d items", len(data))
         es_data: List[Dict[str, Any]] = []
         data_count = 0
-        batch_size = 100
+        chunk_size = 100
         iter_items = iter(data.items())
         doc_id_list: List[int] = []
         while True:
-            chunk = list(islice(iter_items, batch_size))
+            chunk = list(islice(iter_items, chunk_size))
             if not chunk:
                 break
             for inode_num, path_and_size in chunk:
