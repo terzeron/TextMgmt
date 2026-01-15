5eb5ebdd8a1a7a02f950bc8840fdb4d28031008e
Youngil Cho <terzeron@gmail.com>
2026-01-15 13:45:40 +0900

    chore(Dockerfile): Dockerfile을 각 디렉터리로 이동하고 Python 버전 업그레이드
    - Dockerfile.backend와 Dockerfile.frontend를 각각 backend/Dockerfile, frontend/Dockerfile로 이동
    - Python 버전을 3.11에서 3.13으로 업그레이드
    - 기본 이미지를 nexus.nhnent.com/python:3.11-slim에서 python:3.13-slim으로 변경
    - build.sh의 Dockerfile 경로를 새로운 위치로 업데이트

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 rm -rf */{nohup.out*,run.log*,.mypy_cache,__pycache__,.idea,.git}
 
-docker build -f Dockerfile.backend --build-arg TM_BACKEND_PORT="$TM_BACKEND_PORT" -t terzeron/tm_backend . && \
+docker build -f backend/Dockerfile --build-arg TM_BACKEND_PORT="$TM_BACKEND_PORT" -t terzeron/tm_backend . && \
 docker tag terzeron/tm_backend:latest registry.terzeron.com/terzeron/tm_backend:latest && \
 docker push registry.terzeron.com/terzeron/tm_backend:latest
 
-docker build --build-arg VITE_FACEBOOK_APP_ID=$VITE_FACEBOOK_APP_ID --build-arg VITE_API_URL_PREFIX=$VITE_API_URL_PREFIX --build-arg VITE_ADMIN_EMAIL=$VITE_ADMIN_EMAIL -f Dockerfile.frontend -t terzeron/tm_frontend . && \
+docker build --build-arg VITE_FACEBOOK_APP_ID=$VITE_FACEBOOK_APP_ID --build-arg VITE_API_URL_PREFIX=$VITE_API_URL_PREFIX --build-arg VITE_ADMIN_EMAIL=$VITE_ADMIN_EMAIL -f frontend/Dockerfile -t terzeron/tm_frontend . && \
 docker tag terzeron/tm_frontend:latest registry.terzeron.com/terzeron/tm_frontend:latest && \
 docker push registry.terzeron.com/terzeron/tm_frontend:latest
 
