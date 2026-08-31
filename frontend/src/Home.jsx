0613a2f14246607c2c0b9367e9890491b79d9c90
Youngil Cho <terzeron@gmail.com>
2026-08-31 10:58:07 +0900

    사이트 이름 변경

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
         <div>
             <div className="jumbotron jumbotron-fluid">
                 <div className="container mt-3 ms-3">
-                    <h1 className="display-5">은채네 책방</h1>
+                    <h1 className="display-5">우진은채네 책방</h1>
                     <p className="lead">책과 만화가 함께하는 책방에 오신 것을 환영합니다.</p>
                 </div>
             </div>
